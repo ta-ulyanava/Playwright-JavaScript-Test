9bf907e1d6a98c401e9c7f921a64749438c6ed7c
ta-ulyanava <ta.ulyanava@gmail.com>
2024-09-17 22:34:10 +0200

    Add parameterized tests for Salary Insights page (task 3) with role, country, and currency combinations

---
+++ tests/testSalaryInsights.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import SalaryInsights from '../pages/salaryInsights';
+
+const dataset = require("../utils/salaryInsightsTestData");
+
+test.describe('Salary Insights Tests', () => {
+  for (const data of dataset) {
+    test(`Displays salary for selected role ${data.role} in ${data.country}`, async ({ page }) => {
+      const salaryInsites = new SalaryInsights(page);
+
+      // Submit form
+      await salaryInsites.navigate();
+      await salaryInsites.selectRoleOptionFromDropdown(data.role);
+      await salaryInsites.selectCountryOptionFromDropdown(data.country);
+      await salaryInsites.searchButton.click();
+
+      // Assertions
+      await salaryInsites.verifyFilterBarForRoleCountryAndCurrencyCode(data.role, data.country, data.currency.code);
+      await salaryInsites.verifySalaryTableForRoleCountryAndCurrencySymbol(data.role, data.country, data.currency.symbol);
+      await salaryInsites.verifyPromoSectionForRoleCountryAndCurrencySymbol(data.role, data.country, data.currency.symbol);
+    });
+  }
+});
