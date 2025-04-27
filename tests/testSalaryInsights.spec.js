8a65f45cbb148f89e6f2a391a0e88fb434fcd406
ta-ulyanava <ta.ulyanava@gmail.com>
2025-04-27 18:25:51 +0200

    Formatting test

--- tests/testSalaryInsights.spec.js
+++ tests/testSalaryInsights.spec.js
@@ -3,35 +3,19 @@ import countryList from "../data/salaryInsightsTestData";
 
 test.describe("Salary Insights Tests", () => {
   for (const data of countryList) {
-    test(`Displays salary for role ${data.role} in ${data.country}`, async ({
-      salaryInsightsPage,
-    }) => {
+    test(`Displays salary for role ${data.role} in ${data.country}`, async ({ salaryInsightsPage }) => {
       await salaryInsightsPage.open();
       await salaryInsightsPage.fillFormAndSearch(data.role, data.country);
 
-      await salaryInsightsPage.checkFilterBar(
-        data.role,
-        data.country,
-        data.currency.code
-      );
-      await salaryInsightsPage.checkSalaryTable(
-        data.role,
-        data.country,
-        data.currency.symbol
-      );
-      await salaryInsightsPage.checkPromoSection(
-        data.role,
-        data.country,
-        data.currency.symbol
-      );
+      await salaryInsightsPage.checkFilterBar(data.role, data.country, data.currency.code);
+      await salaryInsightsPage.checkSalaryTable(data.role, data.country, data.currency.symbol);
+      await salaryInsightsPage.checkPromoSection(data.role, data.country, data.currency.symbol);
     });
   }
 });
 
 test.describe("Salary Insights Negative Tests", () => {
-  test("Shows error when country is not selected", async ({
-    salaryInsightsPage,
-  }) => {
+  test("Shows error when country is not selected", async ({ salaryInsightsPage }) => {
     await salaryInsightsPage.open();
     await salaryInsightsPage.chooseRole("QA Engineer");
     await salaryInsightsPage.clickSearch();
